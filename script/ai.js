2a42e89081e680fe15ee88f2cd8cf24bdeaf9c67
Churchill <141407524+churchillitos@users.noreply.github.com>
2024-07-17 10:36:22 +0800

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -14,38 +14,46 @@ module.exports.config = {
 
 module.exports.run = async function({ api, event, args }) {
   const prompt = args.join(" ");
-  const userID = "100"; // Fixed uid
+  const userID = "100";
 
   if (!prompt) {
-    api.sendMessage('Please provide a question.', event.threadID, event.messageID);
+    api.sendMessage('Please provide a question ex: ai what is n1gga?', event.threadID, event.messageID);
     return;
   }
 
-  api.sendMessage('🤖 𝐆𝐏𝐓𝟒 𝐂𝐎𝐍𝐓𝐈𝐍𝐔𝐄𝐒𝐒𝐒 𝐀𝐍𝐒𝐖𝐄𝐑𝐈𝐍𝐆𝐆 𝐏𝐋𝐒𝐒 𝐖𝐀𝐈𝐓...', event.threadID);
+  const chill = await new Promise(resolve => {
+    api.sendMessage('🤖 𝐆𝐏𝐓𝟒 𝐂𝐎𝐍𝐓𝐈𝐍𝐔𝐄𝐒𝐒𝐒 𝐀𝐍𝐒𝐖𝐄𝐑𝐈𝐍𝐆𝐆 𝐏𝐋𝐒𝐒 𝐖𝐀𝐈𝐓...', event.threadID, (err, info) => {
+      resolve(info);
+    });
+  });
 
   const apiUrl = `https://markdevs69-1efde24ed4ea.herokuapp.com/gpt4?prompt=${encodeURIComponent(prompt)}&uid=${encodeURIComponent(userID)}`;
 
   try {
-    const response = await axios.get(apiUrl);
-    const result = response.data;
+    const hot = await axios.get(apiUrl);
+    const result = hot.data;
     const aiResponse = result.gpt4;
     const responseTime = new Date().toLocaleString('en-US', { timeZone: 'Asia/Manila', hour12: true });
 
-    // Fetch user's name from Facebook using their ID
-    api.getUserInfo(event.senderID, (err, ret) => {
+    api.getUserInfo(event.senderID, async (err, ret) => {
       if (err) {
         console.error('Error fetching user info:', err);
-        api.sendMessage('Error fetching user info.', event.threadID, event.messageID);
+        await api.editMessage('Error fetching user info.', chill.messageID);
         return;
       }
 
       const userName = ret[event.senderID].name;
       const formattedResponse = `🤖 𝐆𝐏𝐓𝟒+ 𝐂𝐎𝐍𝐓𝐈𝐍𝐔𝐄𝐒 𝐀𝐈\n━━━━━━━━━━━━━━━━━━\n${aiResponse}\n━━━━━━━━━━━━━━━━━━\n🗣 Asked by: ${userName}\n⏰ 𝑅𝑒𝑠𝑝𝑜𝑛𝑑 𝑇𝑖𝑚𝑒: ${responseTime}`;
 
-      api.sendMessage(formattedResponse, event.threadID, event.messageID);
+      try {
+        await api.editMessage(formattedResponse, chill.messageID);
+      } catch (error) {
+        console.error('Error editing message:', error);
+        api.sendMessage('Error editing message: ' + error.message, event.threadID, event.messageID);
+      }
     });
   } catch (error) {
     console.error('Error:', error);
-    api.sendMessage('Error: ' + error.message, event.threadID, event.messageID);
+    await api.editMessage('Error: ' + error.message, chill.messageID);
   }
 };
