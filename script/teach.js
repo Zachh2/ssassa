e3ac84532ccdb01ea220508748d71f6dacc331f0
Churchill <141407524+churchillitos@users.noreply.github.com>
2024-07-24 19:56:05 +0800

    Update teach.js

--- script/teach.js
+++ script/teach.js
@@ -1,30 +1,35 @@
 const axios = require("axios");
 
 module.exports.config = {
-	name: "teach",
-	version: "1",
-	role: 0,
-	credits: "Grey | api by jerome",
-	hasPrefix: false,
-	description: "Teach Simsimi",
-	usage: "Teach",
-	cooldown: 0
+    name: "teach",
+    version: "1.0.0",
+    hasPermission: 0,
+    credits: "jerome",
+    description: "Teach Simsimi",
+    usages: "Teach",
+    commandCategory: "...",
+    cooldowns: 0
 };
 
-module.exports.run = async ({ api, event, args, prefix }) => {
-	try {
-		const text = args.join(" ");
-		const text1 = text.substr(0, text.indexOf(' => '));
-		const text2 = text.split(" => ").pop();
+module.exports.run = async ({ api, event, args }) => {
+    try {
+        const text = args.join(" ");
+        const text1 = text.substr(0, text.indexOf(' - '));
+        const text2 = text.split(" - ").pop();
 
-		if (!text1 || !text2) {
-			return api.sendMessage(`Usage: ${prefix}teach hi => hello`, event.threadID, event.messageID);
-		}
+        if (!text1 || !text2) {
+            return api.sendMessage(`Usage: ${global.config.PREFIX}teach hi - hello`, event.threadID, event.messageID);
+        }
 
-		const response = await axios.get(`https://sim-api-ctqz.onrender.com/teach?ask=${encodeURIComponent(text1)}&ans=${encodeURIComponent(text2)}`);
-		api.sendMessage(`Your ask: ${text1}\nSim respond: ${text2}\nSuccesfull teach`, event.threadID, event.messageID);
-	} catch (error) {
-		console.error("An error occurred:", error);
-		api.sendMessage("Please provide both a question and an answer\nExample: Teach hi => hello", event.threadID, event.messageID);
-	}
+        const response = await axios.get(`https://simsimi-api-pro.onrender.com/teach?ask=${encodeURIComponent(text1)}&ans=${encodeURIComponent(text2)}`);
+
+        if (response.data.respond.includes("already exists")) {
+            return api.sendMessage(`Sim API has already learned this phrase.`, event.threadID, event.messageID);
+        }
+
+        api.sendMessage(`Your ask: ${text1}\nSim respond: ${text2}`, event.threadID, event.messageID);
+    } catch (error) {
+        console.error("An error occurred:", error);
+        api.sendMessage("Oops! Something went wrong.", event.threadID, event.messageID);
+    }
 };
